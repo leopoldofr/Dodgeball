13eaf781bb59b00235388b424629bfc371b4c309
Leo <leopoldo@hotmail.fr>
2016-08-23 11:53:39 +0200

    Ajout du Gitignore + modifs emplacement images

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("Launching Game");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
